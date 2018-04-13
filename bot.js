577153040bbabe2447b24eec0244b62595dc9be6
Dennis George <djg365@drexel.edu>
2018-04-12 22:40:33 -0400

    init

---
+++ bot.js
@@ -0,0 +1,60 @@
+console.log("The bot is starting")
+
+var Twit = require('twit');
+
+var config = require('./config');
+
+var T = new Twit(config);
+
+num = 10;
+sendTweet()
+setInterval(sendTweet, 1000*30);
+
+var stream = T.stream('user');
+
+stream.on('follow', followed);
+
+function followed(eventMsg) {
+	console.log('followed');
+	var name = eventMsg.source.name;
+	var screenName = eventMsg.source.screen_name;
+	sendThanksTweet('Hey @' + screenName + ' thanks for following me!');
+}
+
+function sendTweet() {
+	num = num + 1;
+	var tweet = {
+		status:'current number:' + num
+	}
+
+	T.post('statuses/update', tweet , tweeted);
+
+	function tweeted(err, data, response) {
+	  if(err){
+	  	console.log('Something went wrong');
+	  }
+	  else{
+	  	console.log('It worked!');
+	  }
+	}
+}
+
+function sendThanksTweet(txt) {
+	
+	var tweet = {
+		status: txt
+	}
+
+	T.post('statuses/update', tweet , tweeted);
+
+	function tweeted(err, data, response) {
+	  if(err){
+	  	console.log('Something went wrong');
+	  }
+	  else{
+	  	console.log('It worked!');
+	  }
+	}
+}
+
+
---
+++ config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+	consumer_key:         'aagpmqIU6cHTb9RDKeE9oYCrg',
+ 	consumer_secret:      'fcly9BKFmIwsh4bkuUiEGglRapbYRG0KiDMPoiSw1cU4EAJhch',
+  	access_token:         '984606816284880896-sLGAv08mP2YCpXzpspqiA3Hg7Q4UIv2',
+  	access_token_secret:  'U5fbBKXNmqRibXWXO92G3d7hgDnmutRrhEVOqqJTCvKhg',
+  	//timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
+}
